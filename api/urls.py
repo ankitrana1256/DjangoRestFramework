d4c622dc838f4cc6c908ffe2a0a77db1d5537606
ankitrana1256 <76605774+ankitrana1256@users.noreply.github.com>
2021-09-13 00:50:34 +0530

    crud operations

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("", views.apiOverview, name="api-overview"),
+    path("task-list/", views.taskList, name="task-list"),
+    path("task-detail/<str:pk>", views.taskDetail, name="task-detail"),
+    path("task-create/", views.taskCreate, name="task-create"),
+    path("task-update/<str:pk>", views.taskUpdate, name="task-update"),
+    path("task-delete/<str:pk>", views.taskDelete, name="task-delete"),
+]
